9487ee59c539a1c8d0dc6464561625a54bf33ec1
masakinihirota <masakinihirota@gmail.com>
2024-03-23 14:44:49 +0900

    Storybookの設定を更新しました

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm'
   ],
   framework: {
     name: '@storybook/nextjs',
